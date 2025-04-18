0ff7650ff006ea1a6de840613bdc3328bd8ff9df
ZACK7870 <zack7870@gmail.com>
2025-04-18 16:52:27 +0200

    First commit

---
+++ app/src/main/java/com/example/testapp3aimoc/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.testapp3aimoc
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
